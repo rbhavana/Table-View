9be0c741773918b9e1bc5b216b1804827bb51780
Student <student@BLUC02MG2B8FH51.local>
2016-01-15 11:06:12 -0600

    tableview editing features

--- Table View Practice/ViewController.swift
+++ Table View Practice/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
     @IBOutlet weak var myTableView: UITableView!
-    var superheros = ["Batman", "Wonder Woman", "Superman", "Flash", "Aquaman", "Scuba Steve"]
+    var superHeros = ["Batman", "Wonder Woman", "Superman", "Flash", "Aquaman", "Scuba Steve"]
     var realNames = ["Bruce Wayne", "Diana", "Clark Kent", "Barry Alen", "Arthur Curry", "Steve"]
 
     override func viewDidLoad()
@@ -21,18 +21,78 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         myTableView.dataSource = self
         myTableView.delegate = self
     }
+    @IBAction func addButtonTapped(sender: UIBarButtonItem)
+    {
+        let myAlert = UIAlertController(title: "Add Superhero", message: nil, preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        myAlert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (addAction) -> Void in
+            let superheroNameTextField = myAlert.textFields! [0] as UITextField
+            let aliasTextField = myAlert.textFields! [1] as UITextField
+            self.superHeros.append(superheroNameTextField.text!)
+            self.realNames.append(aliasTextField.text!)
+            self.myTableView.reloadData()
+        }
+        
+        myAlert.addAction(addAction)
+        
+        myAlert.addTextFieldWithConfigurationHandler { (superheroTextField) -> Void in
+            superheroTextField.placeholder = "Add Superhero Name"
+        }
+        myAlert.addTextFieldWithConfigurationHandler { (aliasTextField) -> Void in
+            aliasTextField.placeholder = "Add Real Name"
+        }
+        
+        //presentsthe alert view controller.
+        self.presentViewController(myAlert, animated: true, completion: nil)
+    }
     // creating a cell that would store your data on a tableview
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let myTableViewCell = myTableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as UITableViewCell
-        myTableViewCell.textLabel?.text = superheros[indexPath.row]
+        myTableViewCell.textLabel?.text = superHeros[indexPath.row]
         myTableViewCell.detailTextLabel?.text = realNames[indexPath.row]
         return myTableViewCell
     }
     // sets the number of rows in your tableview
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return superheros.count
+        return superHeros.count
+    }
+    
+    //allows you to delete rows from the tableview
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete
+        {
+            superHeros.removeAtIndex(indexPath.row)
+            realNames.removeAtIndex(indexPath.row)
+            myTableView.reloadData()
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
